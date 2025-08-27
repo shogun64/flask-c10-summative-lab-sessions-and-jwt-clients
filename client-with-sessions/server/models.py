09d1acef69b1d595dc0b9c443128c16ba1d85532
Ian Synge <gamersynge@gmail.com>
2025-08-27 16:14:43 -0400

    Wrote models for User and JournalEntry

--- client-with-sessions/server/models.py
+++ client-with-sessions/server/models.py
@@ -3,7 +3,48 @@ from sqlalchemy.ext.hybrid import hybrid_property
 from config import db, bcrypt
 
 class User(db.Model):
-    pass
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=True)
+
+    journal_entries = db.relationship('JournalEntry', back_populates='user', 
+                    cascade='all, delete-orphan', lazy='joined')
+    
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password hashes may not be viewed.")
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        self._password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+
+    @validates('username')
+    def validate_username(self, key, username):
+        if not username:
+            raise ValueError("Username must be present")
+        return username
+
+    def __repr__(self):
+        return f"<User {self.username}>"
 
 class JournalEntry(db.Model):
-    pass
+    __tablename__ = "journal_entries"
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    contents = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    user = db.relationship('User', back_populates='journal_entries')
+
+    @validates('contents')
+    def validate_contents(self, key, contents):
+        if not contents or len(contents) < 50:
+            raise ValueError(
+                "Contents must contain at least 50 characters")
+        return contents
